06b49a75750670d83db18b863eb76e0514371eaf
oscar <meet@oscarfdez.com>
2022-05-05 23:48:48 +0200

    ejercicio ciclos y simulador interactivo

--- OSCAR_FERNANDEZ-03_ciclos-iteraciones/js/miarchivo.js
+++ OSCAR_FERNANDEZ-03_ciclos-iteraciones/js/miarchivo.js
@@ -1,69 +1,14 @@
-let nombre = prompt("¡Hola! ¿Cómo te llamas?\nEscribe 'esc' para salir").toLowerCase();
-let insulina;
-let ingresarAlimento;
-let calculoRaciones;
-let gramosAlimento;
-let confirmInsulina;
-let continuar;
 
-
-//gramos de cada alimento equivalentes a una ración
-const pan = 20; // 20g de pan equivalen a 1 ración --> si me como 5 raciones de pan son 100g de pan en total
-const manzana = 100;
-const arroz = 15;
-
-//Pedir que alimento va a consumir
-function alimento(){
-    ingresarAlimento = prompt(nombre+ ", Ingresa el alimento que vas a consumir\n-Pan\n-Manzana\n-Arroz").toLowerCase();
+//juego de adivinar la edad
+let edad = parseInt(prompt("¿Cuantos años tiene la reina de Inglaterra?"));
+while(edad != 96 ){
+    alert("La reina de Inglaterra NO tiene " + edad + ". Prueba otra vez.");
+    edad = parseInt(prompt("¿Cuantos años tiene la reina de Inglaterra?"));
 }
 
-//Preguntar si quiere finalizar o volver a empezar
-function continuarCalculo(){
-    continuar = confirm("Continua para volver a emprezar o pulsa cancelar para finalizar")
-    if(continuar == true){
-        nombre = prompt("¡Hola! ¿Cómo te llamas?\nEscribe 'esc' para salir").toLowerCase();
-    } else{
-        nombre = "esc"
-        ingresarAlimento = alert("Gracias")
-    }
-}
+//Juego de deletrear
+for (let i = 1; i <= 5; i++) {
+    let = deletreaMessi = prompt("Juego de deletrear.\nDeletrea Messi")
+    console.log("Letra "+ i +": " + deletreaMessi)
 
-//Calculo de la insulina
-function calculoInsulina() {
-    confirmInsulina = confirm("¿Te gustaría saber cuanta insulina necesitas?")
-    if (confirmInsulina == true) {
-        insulina = parseFloat(prompt("¿Cuál es tu dosis de insulina?\nEscoge tu dosis por ración:\n3\n5\n7"))
-        alert("Para este alimento con este peso necesitas " + insulina * calculoRaciones + " dosis de insulina")
-    }
 }
-
-while(nombre !== "esc") {
-    //Switch comprobando el alimento para hacer el calculo de las raciones de hc
-    alimento()
-    switch (ingresarAlimento) {
-        case "pan":
-            gramosAlimento = parseFloat(prompt("Ingresa los gramos del alimento que vas a consumir"));
-            calculoRaciones = gramosAlimento / pan;
-            alert(gramosAlimento +"g de " + ingresarAlimento + " equivalen a "+ calculoRaciones + " raciones" )
-            calculoInsulina();
-        break;
-        case "manzana":
-            gramosAlimento = parseFloat(prompt("Ingresa los gramos del alimento que vas a consumir"));
-            calculoRaciones = gramosAlimento / manzana;
-            alert(gramosAlimento +"g de " + ingresarAlimento + " equivalen a "+ calculoRaciones + " raciones" )
-            calculoInsulina();
-        break;
-        case "arroz":
-            gramosAlimento = parseFloat(prompt("Ingresa los gramos del alimento que vas a consumir"));
-            calculoRaciones = gramosAlimento / arroz;
-            alert(gramosAlimento +"g de " + ingresarAlimento + " equivalen a "+ calculoRaciones + " raciones" )
-            calculoInsulina();
-        break;
-    
-        default:
-            alert("Por favor, ingresa un alimento válido")
-            break;
-        }
-    continuarCalculo()
-}
-

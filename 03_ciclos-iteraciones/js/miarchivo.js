f4085af627381a55055fe0a6c9db798376d8eae3
oscar <meet@oscarfdez.com>
2022-05-05 20:48:56 +0200

    calcular insulina

--- 03_ciclos-iteraciones/js/miarchivo.js
+++ 03_ciclos-iteraciones/js/miarchivo.js
@@ -1,28 +1,23 @@
-//Pedir nombre para personalizar la experiencia de usuario
-//Calcular las raciones de Hidratos de carbono que se va a consumir según el alimento elegido.
-//Posteriormente dar la opción de calcular las dosis de insulina
 let nombre = prompt("¡Hola! ¿Cómo te llamas?\nEscribe 'esc' para salir").toLowerCase();
-let resultado;
+let insulina;
 let ingresarAlimento;
 let calculoRaciones;
 let gramosAlimento;
 let confirmInsulina;
 let continuar;
 
-function insulina() {
-    confirmInsulina = confirm("¿Te gustaría saber cuanta insulina necesitas?")
-}
 
-//1ración = 10g de HC
-const pan = 20; // 20g de pan equivalen a 1 raciónn --> si me como 5 raciones de pan son 100g de pan en total
-const manzana = 100; // 100g de manzana equivalen a 1 raciónn
-const arroz = 15; // 15g de arroz equivalen a 1 raciónn
+//gramos de cada alimento equivalentes a una ración
+const pan = 20; // 20g de pan equivalen a 1 ración --> si me como 5 raciones de pan son 100g de pan en total
+const manzana = 100;
+const arroz = 15;
 
 //Pedir que alimento va a consumir
-/* function alimento(){
+function alimento(){
     ingresarAlimento = prompt(nombre+ ", Ingresa el alimento que vas a consumir\n-Pan\n-Manzana\n-Arroz").toLowerCase();
-} */
+}
 
+//Preguntar si quiere finalizar o volver a empezar
 function continuarCalculo(){
     continuar = confirm("Continua para volver a emprezar o pulsa cancelar para finalizar")
     if(continuar == true){
@@ -33,27 +28,36 @@ function continuarCalculo(){
     }
 }
 
+//Calculo de la insulina
+function calculoInsulina() {
+    confirmInsulina = confirm("¿Te gustaría saber cuanta insulina necesitas?")
+    if (confirmInsulina == true) {
+        insulina = parseFloat(prompt("¿Cuál es tu dosis de insulina?\nEscoge tu dosis por ración:\n3\n5\n7"))
+        alert("Para este alimento con este peso necesitas " + insulina * calculoRaciones + " dosis de insulina")
+    }
+}
+
 while(nombre !== "esc") {
-    //Switch comprobando el alimento para hacer el calculo
+    //Switch comprobando el alimento para hacer el calculo de las raciones de hc
     alimento()
     switch (ingresarAlimento) {
         case "pan":
             gramosAlimento = parseFloat(prompt("Ingresa los gramos del alimento que vas a consumir"));
             calculoRaciones = gramosAlimento / pan;
-            resultado = calculoRaciones;
             alert(gramosAlimento +"g de " + ingresarAlimento + " equivalen a "+ calculoRaciones + " raciones" )
+            calculoInsulina();
         break;
         case "manzana":
             gramosAlimento = parseFloat(prompt("Ingresa los gramos del alimento que vas a consumir"));
             calculoRaciones = gramosAlimento / manzana;
-            resultado = calculoRaciones;
             alert(gramosAlimento +"g de " + ingresarAlimento + " equivalen a "+ calculoRaciones + " raciones" )
+            calculoInsulina();
         break;
         case "arroz":
             gramosAlimento = parseFloat(prompt("Ingresa los gramos del alimento que vas a consumir"));
             calculoRaciones = gramosAlimento / arroz;
-            resultado = calculoRaciones;
             alert(gramosAlimento +"g de " + ingresarAlimento + " equivalen a "+ calculoRaciones + " raciones" )
+            calculoInsulina();
         break;
     
         default:
